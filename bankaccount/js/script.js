298fede1d23ef0c627aae3f9bc000062d8396515
jmaine22 <jmaine22@gmail.com>
2021-05-20 01:31:11 -0700

    assignment13

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -1,19 +1,21 @@
 
-(function myTransaction(){
-    let name = document.querySelector("#name");
-    let deposit = document.querySelector("#deposit");
-    let withdraw = document.querySelector("#withdraw");
+function myTransaction(){
+    let name = document.getElementById('name');
+    let deposit = document.getElementById('deposit');
+    let withdraw = document.getElementById('withdraw');
 
+    init();
     function init() {
         name.addEventListener('click', function () {
             prompt("Enter your Name");
         });
         deposit.addEventListener('click', function () {
             prompt("Amount to deposit");
-        })
+        });
         withdraw.addEventListener('click', function () {
             prompt("Amount to withdraw");
-        })
+        });
     }
 //
-})
+}
+myTransaction();
